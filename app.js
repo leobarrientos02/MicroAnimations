7c976c9db4450b3e080f1daef91826aa3e8414e7
Your Name <leobarrientos02@gmail.com>
2022-01-10 15:47:16 -0500

    bell SVG animation

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const tl = gsap.timeline({
     ease: "Power2.easeOut",
   },
 });
+
+// HOME
 const home = document.querySelector(".home");
+// NOTIFICATION
+const notification = document.querySelector(".notification");
 
 gsap.set(".feather", { scale: 0, transfromOrgin: "center" });
 
@@ -41,6 +45,58 @@ home.addEventListener("click", () => {
     },
     {
       x: 5,
+    }
+  );
+});
+
+// NOTIFICATION
+gsap.set(".bell", {
+  transfromOrgin: "top center",
+});
+// SETTING THE TRANSFORMORGIN
+gsap.set(".ringer", {
+  transfromOrgin: "top center",
+});
+gsap.set(".wave", {
+  opacity: 0,
+  transfromOrgin: "bottom",
+});
+
+notification.addEventListener("click", () => {
+  gsap.fromTo(
+    ".bell",
+    {
+      rotation: -5,
+    },
+    {
+      rotation: 0,
+      duration: 2,
+      ease: "elastic.out(5, 0.2)",
+    }
+  );
+  gsap.fromTo(
+    ".ringer",
+    {
+      rotation: -3,
+      x: 0.5,
+    },
+    {
+      rotation: 0,
+      x: 0,
+      duration: 1,
+      ease: "elastic.out(5, 0.2)",
+    }
+  );
+  gsap.fromTo(
+    ".wave",
+    {
+      scale: 0,
+      opacity: 1,
+    },
+    {
+      scale: 1.3,
+      opacity: 0,
+      duration: 1.2,
     }
   );
 });
