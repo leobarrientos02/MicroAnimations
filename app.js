31c24c763bde109e254cb30f9aa84210c4e463ef
Your Name <leobarrientos02@gmail.com>
2022-01-09 22:55:38 -0500

    home svg animation

---
+++ app.js
@@ -0,0 +1,46 @@
+const tl = gsap.timeline({
+  defaults: {
+    duration: 0.35,
+    ease: "Power2.easeOut",
+  },
+});
+const home = document.querySelector(".home");
+
+gsap.set(".feather", { scale: 0, transfromOrgin: "center" });
+
+home.addEventListener("click", () => {
+  gsap.fromTo(
+    ".home-svg",
+    {
+      scale: 1,
+    },
+    {
+      scale: 0.9,
+      yoyo: true,
+      repeat: 1,
+    }
+  );
+
+  gsap.fromTo(
+    ".feather",
+    {
+      y: -5,
+      scale: 0,
+    },
+    {
+      y: 20,
+      scale: 1.5,
+      duration: 3,
+      stagger: 0.4,
+    }
+  );
+  gsap.fromTo(
+    ".right-feather",
+    {
+      x: 0,
+    },
+    {
+      x: 5,
+    }
+  );
+});
